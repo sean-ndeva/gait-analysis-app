4f8504245fcc0a6ed46c30d755d2e0f98fc6ea57
sean-ndeva <seanndeva0@gmail.com>
2025-11-03 17:36:35 +0300

    Professional UI update and deployment ready

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"/c/Users/SPECTRE/AppData/Local/Programs/Python/Python312/python.exe" -m streamlit run app.py
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# setup.sh - For Streamlit Community Cloud
+pip install -r requirements.txt
